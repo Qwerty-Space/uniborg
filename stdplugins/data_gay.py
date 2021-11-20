5a2ac31fa891864bf1cf01545c25203afa575fd6
Qwerty-Space <oliver_duce@live.co.uk>
2021-11-20 00:20:52 +0000

    make work

--- stdplugins/data_gay.py
+++ stdplugins/data_gay.py
@@ -6,6 +6,6 @@ import asyncio
 from telethon import events
 
 
-@borg.on(events.NewMessage(incoming=True, chats=789995065))
+@borg.on(events.NewMessage(incoming=True, chats=[789995065]))
 async def _(event):
-    event.mark_read()
+    await event.mark_read()
