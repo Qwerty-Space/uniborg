08e711b4215aff3687af44beaff412934da01dfa
Qwerty-Space <oliver_duce@live.co.uk>
2025-10-11 11:41:47 +0100

    move 8ball to its own plugin and make weights match original design

---
+++ botplugins/8ball.py
@@ -0,0 +1,65 @@
+r"""Magic 8 Ball
+
+Responds with a magic eight-ball answer to `/8ball`
+
+patterns:
+`/8ball( .+)?`
+"""
+
+import re
+from random import choice, randint
+
+from telethon import events
+from uniborg.util import blacklist
+
+
+positive_answers = [
+    "Without a doubt.",
+    "Yes.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "Signs point to yes.",
+    "Outlook good.",
+    "It is certain.",
+    "It is decidedly so.",
+    "Most likely.",
+    "As I see it, yes.",
+]
+
+neutral_answers = [
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+]
+
+negative_answers = [
+    "Very doubtful.",
+    "Very unlikely.",
+    "Outlook not so good.",
+    "My reply is no.",
+    "My sources say no.",
+    "No.",
+    "No - don't even think about it.",
+    "Don't count on it.",
+]
+
+# Matches "/8ball" 
+@borg.on(borg.cmd(r"8ball( .+)?"))
+async def magic(event):
+    blacklist = storage.blacklist or set()
+    if event.chat_id in blacklist:
+        return
+
+    answers_list = [
+        positive_answers, positive_answers,
+        neutral_answers, negative_answers
+    ]
+
+    outcome = randint(0, 3)
+    await event.reply(choice(answers_list[outcome]))
+
+@borg.on(borg.blacklist_plugin())
+async def on_blacklist(event):
+    storage.blacklist = await blacklist(event, storage.blacklist)
--- botplugins/yes_no.py
+++ botplugins/yes_no.py
@@ -3,11 +3,8 @@ r"""Yes...  Or No?
 Adding "yes or no" to the end of your message will result in a yes or no answer from the bot.
 Also supports "y/n", and other alternatives.
 
-Will also respond with a magic eight-ball answer to `/8ball`
-
 patterns:
 `(?i)(yes|y)(/| or )(no|n)\??$`
-`/8ball`
 """
 
 import re
@@ -47,7 +44,6 @@ answers = [
 # Matches "y/n" "yes or no", "yes/no?", "/8ball" etc
 @borg.on(events.NewMessage(
     pattern=re.compile(r"(?i)(yes|y)(/| or )(no|n)\??$").search, forwards=False))
-@borg.on(borg.cmd(r"8ball( .+)?"))
 async def yes_or_no(event):
     blacklist = storage.blacklist or set()
     if event.chat_id in blacklist:
