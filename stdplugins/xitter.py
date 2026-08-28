8da9e856fd2f52162fc7ba9bf57cf93955d267be
Qwerty-Space <oliver_duce@live.co.uk>
2026-08-28 21:26:06 +0100

    merge from upstream and hide fixupx link

--- stdplugins/xitter.py
+++ stdplugins/xitter.py
@@ -5,9 +5,32 @@ Replaces twitter links with fixupx links
 import re
 from telethon import events
 
-@borg.on(events.NewMessage(outgoing=True, pattern=re.compile(r"\b((?:twitter|x)\.com)(?=\/\S+)").search))
+domains = "|".join([
+    "x",
+    "fixupx",
+    "xfixup",
+    "fixvx",
+
+    "twitter",
+    "fxtwitter",
+    "twittpr",
+    "vxtwitter",
+
+    "boypussyx",
+    "cunnyx",
+    "furryfeetx",
+    "girlcockx",
+    "hitlerx",
+    "stupidpenisx",
+])
+
+@borg.on(events.NewMessage(outgoing=True,
+    pattern=re.compile(fr"\b(?:https?://)?(?:www\.)?(?:{domains})\.com/(?P<path>(?:i|[a-zA-Z0-9_]{{4,15}})/status/\d+)(?:\?\S*)?\b").search))
 async def xitter(event):
-    m = event.pattern_match.group(1)
-    new_text = (event.raw_text).replace(m, "fixupx.com")
+    m = event.pattern_match.group(0)
+    path = event.pattern_match.group(1)
+    new_text = (event.raw_text).replace(m, f"[\u2063](https://fixupx.com/{path})https://x.com/{path}")
+    if new_text == event.raw_text:
+        return
     await event.edit(new_text, link_preview=True)
 
