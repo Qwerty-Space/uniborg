ef77b31b8588ba1f8b657f9528cc59e07973f5a9
Qwerty-Space <oliver_duce@live.co.uk>
2026-09-04 21:49:38 +0100

    add support for lang codes, add preview hyperlink and refactor groups

--- stdplugins/xitter.py
+++ stdplugins/xitter.py
@@ -27,11 +27,12 @@ domains = "|".join([
 ])
 
 @borg.on(events.NewMessage(outgoing=True,
-    pattern=re.compile(fr"\b(?:https?://)?(?:www\.)?(?:{domains})\.com/(?P<path>(?:i|[a-zA-Z0-9_]{{4,15}})/status/\d+)(?:\?\S*)?\b").search))
+    pattern=re.compile(fr"\b(?:https?://)?(?:www\.)?(?:{domains})\.com/(?P<path>(?:i|[a-zA-Z0-9_]{{4,15}})/status/\d+)(?P<lang>/\w{{2}})?(?:\?\S*)?\b").search))
 async def xitter(event):
     m = event.pattern_match.group(0)
-    path = event.pattern_match.group(1)
-    new_text = (event.raw_text).replace(m, f"[\u2063](https://fixupx.com/{path})https://x.com/{path}")
+    path = event.pattern_match.group("path")
+    lang = event.pattern_match.group("lang") or ""
+    new_text = (event.raw_text).replace(m, f"[(preview)](https://fixupx.com/{path}{lang}) https://x.com/{path}")
     if new_text == event.raw_text:
         return
     await event.edit(new_text, link_preview=True)
